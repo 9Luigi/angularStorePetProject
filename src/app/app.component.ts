d6f653bb7ac0e7b8b75e9e3bff6d1eceb4ee7aba
Kudrik Roman <fastromankudrik@gmail.com>
2023-04-05 15:04:07 +0600

    pipe filter , loading animation, error comp added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/product';
 import { HttpProductsService } from './services/productsHttp.service'
-import { Observable, catchError, delay, filter, first, take, tap, throwError } from 'rxjs';
-import { HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Observable, catchError, delay, retry, tap, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ErrorService } from './services/error.service';
 
 @Component({
   selector: 'app-root',
@@ -10,22 +11,25 @@ import { HttpErrorResponse, HttpParams } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private httpService: HttpProductsService) { }
-
-  title = 'angularStorePetProject';
+  constructor(private httpService: HttpProductsService, private errorService: ErrorService) { }
+  condition: string = '';
+  title = 'My store';
   loading: boolean = false;
   products$: Observable<IProduct[]>
-
   ngOnInit(): void {
     this.loading = true;
-    this.products$ = this.httpService.getProducts().pipe(delay(2000), catchError(this.errorHandler));
-    this.loading = false;
+    this.products$ = this.httpService.getProducts().pipe(
+      delay(1),
+      retry(2),
+      catchError(this.errorHandler.bind(this)),
+      tap(() => { this.loading = false })
+    );
   }
 
   private errorHandler(error: HttpErrorResponse) {
+    this.errorService.handle(error.message);
     return throwError(() => {
       error.message;
     })
   }
-
 }
